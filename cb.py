87b0f72dc2704dba0e2b1e8bec49a931511c21f7
sunil-2000 <sunilsabnis10@gmail.com>
2023-03-17 12:37:57 -0400

    generate regular gpt predictions in batch

--- cb.py
+++ cb.py
@@ -1,15 +1,19 @@
 import torch
+from datasets import Dataset
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from transformers import AutoTokenizer, GPT2LMHeadModel
-
 
 class CB:
     """
     coherence-boosted GPT model
     """
 
-    def __init__(self, alpha, k, model_id="gpt2"):
+    def __init__(self, alpha, k, model_id="gpt2", device='cuda'):
+        self.device = torch.device(device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.tokenizer.padding_side = "left" 
         self.model = GPT2LMHeadModel.from_pretrained(model_id)
+        self.model.to(self.device)
         self.alpha = alpha
         self.k = k
 
@@ -55,8 +59,95 @@ class CB:
         )
         return self.tokenizer.decode(output["scores"][0].argmax())
 
-    def set_attn_mask(self):
+    def batched_generate(self, X, batch_size=32):
         """
-        set attention mask to for gpt inference with batches
+        generate batched predictions and return generated token ids
+        X: 2D list of passages 
         """
-        raise NotImplemented
+        encoded_inputs = self.tokenizer(X, padding=True, return_tensors="pt")
+        encoded_inputs.to(self.device)
+        # generate dataset to process in batches
+        dataset = Dataset.from_dict(encoded_inputs)
+        dataset.set_format(type='torch', columns=['input_ids', 'attention_mask'])
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+        # run predictions in batches
+        # also track scores (so don't need to run model twice for boosted preds)
+        preds = []
+        for i, data in enumerate(dataloader):
+            print(f"processing batch {i}")
+            input_ids, mask = data['input_ids'].to(self.device), data['attention_mask'].to(self.device)
+            out = self.model.generate(input_ids, attention_mask=mask, output_scores=True, return_dict_in_generate=True, max_new_tokens=1, pad_token_id=self.tokenizer.eos_token_id)['scores'][0]
+            pred_token = torch.argmax(out, axis=1)
+            preds.append(pred_token)
+            print("-"*50)
+
+        # flatten preds
+        flat_preds = [pred for batch in preds for pred in batch]
+        return flat_preds
+
+    def k_batched_generate(self, X):
+        """
+        batched generation with shorter context
+        """
+        raise NotImplementedError
+    
+    def boosted_batched_generate(self, X, batch_size=32):
+        """
+        generate boosted batched predictions and return generated token ids
+        X: 2D list of passages
+        """
+        raise NotImplementedError
+        encoded_input = self.tokenizer(X, padding=True, return_tensors="pt")
+        full_mask = encoded_input['attention_mask']
+        n, d = encoded_input['input_ids'].shape
+        print(f'len(X): {len(X)}, encoded_input shape: {encoded_input.shape}')
+        # for i in encoded_input:
+        #     print(i[-self.k:][None, :])
+        #     0/0
+        encoded_short = torch.stack([i[-self.k :] for i in encoded_input])
+        # attend to all but last 10 positions
+        short_mask = torch.zeros(d)
+        short_mask[-self.k:] = 1
+        print(f'encoded_short shape: {encoded_short.shape}')
+
+        # full_input = torch.stack((encoded_input, full_mask), axis=1).to(self.device)
+        # short_input = torch.stack((encoded_short, short_mask), axis=1).to(self.device)
+        # encoded_input = encoded_input.to(self.device)
+        # encoded_short = encoded_short.to(self.device)
+
+
+        # generate dataset to process in batches
+        dataset = TensorDataset(encoded_input)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+        # run predictions in batches
+        max_preds = []
+        boosted_preds = []
+        for i, (data_max) in enumerate(dataloader):
+            print(f"processing batch {i}")
+            out_max = self.model.generate(data_max, output_scores=True, return_dict_in_generate=True, max_new_tokens=1)['scores'][0]
+            print(data_max)
+            0/0
+            out_k = self.model.generate(data_k, output_scores=True, return_dict_in_generate=True, max_new_tokens=1)['scores'][0]
+            
+            # stack out, k
+            out_boost = torch.argmax(out_max + (self.alpha * out_k), axis=1)
+            out_full_ctx = torch.argmax(out_max, axis=1)
+            
+            boosted_preds.append(out_boost)
+            max_preds.append(out_full_ctx)
+            print("-"*50)
+
+        # flatten _preds
+        return max_preds, boosted_preds
+    
+    def generate_dataset(self):
+        pass
+
+    def label_to_token(self, Y):
+        """
+        convert true label to their token representation
+        Y: 1D list of target labels
+        """
+        return self.tokenizer(Y)['inputed_ids']
